ddeb257105fa7bdd0116aca78c43ccf99828e5d3
Presley <presleyanne@gmail.com>
2014-10-04 17:38:13 -0400

    Change structure of TableauSet.
    Candidates are now held in a flat list. Grouping by UR is done in a
    utils function when needed for normalization.

--- candidate.py
+++ candidate.py
@@ -25,7 +25,7 @@ class TableauSet:
         format.'''
         (self.long_constraint_names,
         self.short_constraint_names,
-        self.urs) = self.parse_tableau_file(tableau_file)
+        self.candidates) = self.parse_tableau_file(tableau_file)
         self.set_probabilities()
 
     def parse_tableau_file(self, tableau_file):
@@ -34,30 +34,21 @@ class TableauSet:
         rows = utils.begin_parse(tableau_file)
         long_constraint_names = rows[0][3:]
         short_constraint_names = rows[1][3:]
-        tableau = collections.defaultdict(list)
-        # stateful: ur can be left out, in which case you use the most recent ur
+        candidates = []
+        # # stateful: ur can be left out, in which case you use the most recent ur
         ur = None
         for row in rows[2:]:
             ur = row[0] if row[0] else ur
             sr = row[1]
-            frequency = float(row[2]) if row[2] else 0.0
+            frequency = int(row[2]) if row[2] else 0
             violations = np.array([int(violation) if violation else 0
                 for violation in row[3:]])
-            tableau[ur].append(Candidate(ur, sr, frequency, violations))
-        return long_constraint_names, short_constraint_names, tableau
+            violations = [-v if v >= 0 else v for v in violations]
+            candidates.append(Candidate(ur, sr, frequency, violations))
+        return long_constraint_names, short_constraint_names, candidates
 
     def set_probabilities(self):
-        totals = dict([(ur, sum(utils.pluck(srlist, 'frequency'))) for (ur, srlist) in
-                self.urs.iteritems()])
-        for (ur, srlist) in self.urs.iteritems():
-            for sr in srlist:
-                sr.probability = sr.frequency/totals[ur]
-
-    def all_candidates(self):
-        '''None -> generator of Candidates.
-        Allows you to easily iterate over all UR-SR pairs even though they are
-        stored in a nested data structure. Must be used with care because
-        generators are stateful.'''
-        for ur in self.urs.values():
-            for candidate in ur:
-                yield candidate
+        frequencies = utils.pluck(self.candidates, 'frequency')
+        probabilities = utils.normalize_by_ur(self.candidates, frequencies)
+        for (candidate, probability) in zip(self.candidates, probabilities):
+            candidate.probability = probability
--- utils.py
+++ utils.py
@@ -1,12 +1,32 @@
+from __future__ import division
 import csv
+from itertools import groupby
 
 def begin_parse(input_file):
+    '''String -> [String]. Take a filename for a tab-delimited
+    file and get a list of the rows of the file.'''
     with open(input_file) as f:
         reader = csv.reader(f, delimiter = '\t')
         rows = [row for row in reader]
         return rows
 
 def pluck(objects, attribute):
+    '''Collection, String -> Collection. Like underscore.js's pluck.
+    Gets the specified attribute from each item in the collection.'''
     return map(lambda obj: getattr(obj, attribute), objects)
 
+def normalize_by_ur(candidates, sequence):
+    '''[Candidate], [Number] -> [Number]. Each number in sequence belongs to one
+    candidate, in order. Divides each number by the sum of all those numbers
+    that are associated with the same UR. Useful for normalizing both empirical
+    and predicted probabilities of candidates. A probability normalized by UR
+    represents the probability that the given SR is the right output for its
+    UR.'''
+    labeled_items = zip(candidates, sequence)
+    ur_sums = dict([(ur, sum([item for (cand, item) in group]))
+          for (ur, group) # group is iterator over (candidate, item) pairs
+          in groupby(labeled_items,
+                     lambda (candidate, item): candidate.ur)])
+    return [item / ur_sums[candidate.ur]
+        for (candidate, item) in labeled_items]
 
